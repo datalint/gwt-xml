f9e5a2f8fc336c1e6bf0d6db652fb9e89d7de4a4
Hua Zhang <hzhang1978@gmail.com>
2024-12-28 14:41:56 +0100

    rename pom files in test folder

--- test/server/src/main/java/gwt/xml/test/server/Application.java
+++ test/server/src/main/java/gwt/xml/test/server/Application.java
@@ -31,12 +31,12 @@ public class Application extends SpringBootServletInitializer {
     public static class EmbeddedServletContainerConfig implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
         @Override
         public void customize(TomcatServletWebServerFactory factory) {
-            /**
-             * You have to set a document root here, otherwise RemoteServiceServlet will fail to find the
-             * corresponding serializationPolicyFilePath on a temporary web server started by spring boot application:
-             * servlet.getServletContext().getResourceAsStream(serializationPolicyFilePath) returns null.
-             * This has impact that java.io.Serializable can be no more used in RPC, only IsSerializable works.
-             * */
+            /*
+              You have to set a document root here, otherwise RemoteServiceServlet will fail to find the
+              corresponding serializationPolicyFilePath on a temporary web server started by spring boot application:
+              servlet.getServletContext().getResourceAsStream(serializationPolicyFilePath) returns null.
+              This has impact that java.io.Serializable can be no more used in RPC, only IsSerializable works.
+              */
             factory.setDocumentRoot(new File(Objects.requireNonNull(getClass().getResource("/")).getFile(),
                     "public"));
         }
