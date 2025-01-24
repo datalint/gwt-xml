1ac5e718e8bec934f75fcc86bd70e49712c72be0
Hua Zhang <hzhang1978@gmail.com>
2025-01-24 18:05:45 +0100

    improve getClassLoader

--- shared/src/main/java/gwt/xml/shared/impl/SuperUtil.java
+++ shared/src/main/java/gwt/xml/shared/impl/SuperUtil.java
@@ -8,13 +8,22 @@ public class SuperUtil {
     @SuppressWarnings("unchecked")
     public static <T> T findInstance(String propertyKey, Supplier<T> fallback) {
         try {
-            Class<?> clazz = ClassLoader.getSystemClassLoader().loadClass(System.getProperty(propertyKey));
+            Class<?> clazz = getClassLoader().loadClass(System.getProperty(propertyKey));
 
             MethodHandles.Lookup lookup = MethodHandles.lookup();
             return (T) lookup.findConstructor(clazz, MethodType.methodType(void.class)).invoke();
         } catch (Throwable e) {
             return fallback.get();
         }
+    }
+
+    private static ClassLoader getClassLoader() {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+        if (classLoader == null)
+            return ClassLoader.getSystemClassLoader();
+
+        return classLoader;
     }
 
     private SuperUtil() {
