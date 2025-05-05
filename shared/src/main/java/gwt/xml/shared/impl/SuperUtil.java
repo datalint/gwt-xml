20ce43b52b82aabca79211d3aab202c704cb3995
Hua Zhang <hzhang1978@gmail.com>
2025-05-05 15:47:49 +0200

    introduce own config properties instead of using system get property call, hence get rid of using custom property binding and use environment post processor.

--- shared/src/main/java/gwt/xml/shared/impl/SuperUtil.java
+++ shared/src/main/java/gwt/xml/shared/impl/SuperUtil.java
@@ -1,14 +1,21 @@
 package gwt.xml.shared.impl;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
+import java.util.Properties;
 import java.util.function.Supplier;
 
 public class SuperUtil {
     @SuppressWarnings("unchecked")
     public static <T> T findInstance(String propertyKey, Supplier<T> fallback) {
         try {
-            Class<?> clazz = getClassLoader().loadClass(System.getProperty(propertyKey));
+            Class<?> clazz = getClassLoader().loadClass(ConfigProperties.instance.getProperty(propertyKey));
 
             MethodHandles.Lookup lookup = MethodHandles.lookup();
             return (T) lookup.findConstructor(clazz, MethodType.methodType(void.class)).invoke();
@@ -27,5 +34,28 @@ public class SuperUtil {
     }
 
     private SuperUtil() {
+    }
+
+    private static class ConfigProperties extends Properties {
+        private static final String configProperties = "config.properties";
+
+        private static final ConfigProperties instance = new ConfigProperties();
+
+        private ConfigProperties() {
+            try {
+                Enumeration<URL> resources = getClassLoader().getResources(configProperties);
+                while (resources.hasMoreElements()) {
+                    try (InputStream inputStream = resources.nextElement().openStream()) {
+                        if (inputStream != null) {
+                            load(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+                        }
+                    } catch (IOException e) {
+                        // Ignore
+                    }
+                }
+            } catch (IOException e) {
+                // Ignore
+            }
+        }
     }
 }
--- test/server/src/main/java/gwt/xml/test/server/TestServlet.java
+++ test/server/src/main/java/gwt/xml/test/server/TestServlet.java
@@ -11,6 +11,6 @@ import java.io.IOException;
 @WebServlet({"/test"})
 public class TestServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.getWriter().println("Hello, world (from TestServlet) " + XmlParser.parse("<a b=\"test\"/>"));
+        resp.getWriter().println("Hello, world (from Xml TestServlet) " + XmlParser.parse("<a b=\"xml test\"/>"));
     }
 }
