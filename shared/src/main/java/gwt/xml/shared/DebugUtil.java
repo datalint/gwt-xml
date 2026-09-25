f092ee56fde0e76e558eb5a7b888b5bb921daf01
Hua Zhang <hzhang1978@gmail.com>
2026-09-25 12:42:53 +0200

    refactoring logger/LOGGER to log

--- shared/src/main/java/gwt/xml/shared/DebugUtil.java
+++ shared/src/main/java/gwt/xml/shared/DebugUtil.java
@@ -4,20 +4,20 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DebugUtil {
-    private static final Logger logger = Logger.getLogger(DebugUtil.class.getName());
+    private static final Logger log = Logger.getLogger(DebugUtil.class.getName());
 
     private DebugUtil() {
     }
 
     public static void log(Object message) {
-        logger.log(Level.INFO, String.valueOf(message));
+        log.log(Level.INFO, String.valueOf(message));
     }
 
     public static void log(Throwable caught) {
-        logger.log(Level.WARNING, caught.getMessage(), caught);
+        log.log(Level.WARNING, caught.getMessage(), caught);
     }
 
     public static Logger getLogger() {
-        return logger;
+        return log;
     }
 }
